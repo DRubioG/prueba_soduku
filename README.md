ce9e993025b58e203534cc7373df534e8d524630
DRubioG <35471722+DRubioG@users.noreply.github.com>
2020-07-04 20:26:19 +0200

    dsd
    holasxx

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # prueba_soduku
  El objetivo de este repositorio es detectar las casillas de un sudoku que contienen un número
+
+## Codigo
+### Prueba 1
+Esta prueba consiste en localizar cuales son las casillas que contienen un numero
+
+""" python
+imgR=cv2.imread('imagenes/test6.jpg', 0)
+img=cv2.resize(imgR, (300,300), interpolation=cv2.INTER_LINEAR) #reescalo la imagen a 300x300
+height, width = img.shape[:2] #extraigo la altura y la anchura de la imagen
+ret, imgthres=cv2.threshold(img, thres, 255, cv2.THRESH_BINARY) #aplico el threshold
+cv2.rectangle(imgthres, (1,1), (width-1, height-1), (255, 255, 255), 3) # elimino el contorno que rodea a la imagen
+contours,hierarchy =cv2.findContours(imgthres, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #extraigo los contornos de la imagen del sudoku
+"""
+1. Primero se lee la imagen
+![imagen](imagenes/test.png)
+2. Se reescala para hacer más facil el trabajo
+![imagen](imagenes/imagenes_prueba1/reescalado.jpg)
+3. Se aplica un threshold para eliminar impurezas en la imagen
+![imagen](imagenes/imagenes_prueba1/threshold.jpg)
+4. Se superpone un rectangulo blanco son el sudoku para eliminar los margenes
+5. Se obtienen los contornos de la imagen
+![imagen](imagenes/imagenes_prueba1/contornos.jpg)
+(Para poder ver las casillas se utiliza cambia la imagen original por una RGB)
+"""python
+img=cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+"""
+
--- prueba1.py
+++ prueba1.py
@@ -1,64 +1,60 @@
 import cv2
 import numpy as np
 
-thres=240
+thres=240 #el valor umbral del threshold
 
-imgR=cv2.imread('test.png', 0)
-img=cv2.resize(imgR, (300,300), interpolation=cv2.INTER_LINEAR)
+imgR=cv2.imread('imagenes/test.png', 0)
+img=cv2.resize(imgR, (300,300), interpolation=cv2.INTER_LINEAR) #reescalo la imagen a 300x300
 
-height, width = img.shape[:2]
-#print(height)
-#print(width)
+height, width = img.shape[:2] #extraigo la altura y la anchura de la imagen
 
-#
-ret, imgthres=cv2.threshold(img, thres, 255, cv2.THRESH_BINARY)
+ret, imgthres=cv2.threshold(img, thres, 255, cv2.THRESH_BINARY) #aplico el threshold
 
-cv2.rectangle(imgthres, (1,1), (width-1, height-1), (255, 255, 255), 3)
-
-contours,hierarchy =cv2.findContours(imgthres, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-#print((contours))
+cv2.rectangle(imgthres, (1,1), (width-1, height-1), (255, 255, 255), 3) # elimino el contorno que rodea a la imagen
+contours,hierarchy =cv2.findContours(imgthres, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #extraigo los contornos de la imagen del sudoku
+img=cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+#cv2.drawContours(img, contours, -1, (255, 0,0))
+#cv2.imwrite("imagenes/imagenes_prueba1/contornos.jpg", img)
 areas=[]
 anchos=[]
 i=0
 numero=[]
 num=[]
+
 for con in contours:
-    area=cv2.contourArea(con)
+    area=cv2.contourArea(con) #calculamos el area de cada contorno
     if (area > 200) & (area< 2000):
-        x,y, h, w=cv2.boundingRect(con)
-        #cv2.rectangle(img, (x+4,y+4), (x+w-4, y+h-4), (255, 0, 0), 1)
-        areas.append(con)
-        pru=imgthres[y+4:y+h-4, x+4:x+w-4]
-        
-        media=cv2.mean(pru)[0]
+        x,y, h, w=cv2.boundingRect(con) #si el area del contorno esta entre 200 y 2000 calculamos cual es rectangulo que contiene el contorno
+        cv2.rectangle(img, (x+4,y+4), (x+w-4, y+h-4), (255, 255, 0), 1) #lo pintamos
+        areas.append(con) #añadimos el contorno a la lista de areas
+        pru=imgthres[y+4:y+h-4, x+4:x+w-4] #extraemos del la imagen threshold el contorno que contiene el area
+        media=cv2.mean(pru)[0] #calculamos la media de los pixeles de la imagen
         if  media < 254.9:
-          #print("dentro")
-          #numero.append(con)
+          numero.append(con)
           #pru2=cv2.resize(pru, (300,300), interpolation=cv2.INTER_LINEAR)
-          cv2.rectangle(pru, (1,1), (h-4, w-4), (255, 255, 255), 2)
-          
-          conN, hierN=cv2.findContours(pru, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-          pru=cv2.cvtColor(pru, cv2.COLOR_GRAY2BGR)
-          cv2.drawContours(pru, conN, -1, (0,200,0))
-          cv2.imshow("pru", pru)
-          cv2.waitKey(0)
+          #conN, hierN=cv2.findContours(pru, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+         # pru=cv2.cvtColor(pru, cv2.COLOR_GRAY2BGR)
+          cv2.rectangle(img, (x+4,y+4), (x+w-4, y+h-4), (255, 0, 255), 1)
+          #cv2.drawContours(pru, conN, -1, (0,200,0))
+          #cv2.imshow("pru", pru)
+         # cv2.waitKey(0)
           #num.append(conN)
-          for cont in conN:
+          #for cont in conN:
             #num.append(cont)
             #print(cont[0])
            # imgH=cv2.resize(img, (300*w,300*h), interpolation=cv2.INTER_LINEAR)
-            cv2.drawContours(img, cont+[x+4, y+4], -1, (255, 255,0), 1, 1)
+          #  cv2.drawContours(img, cont+[x+4, y+4], -1, (255, 255,0), 1, 1)
             #
           
           #cv2.drawContours(img, conN, i, (0,255,0), 1)
            # i=i+1
 #for i in range(len(conN)):
 #print(len(num))
-imgthres=cv2.cvtColor(imgthres, cv2.COLOR_GRAY2BGR)
+#imgthres=cv2.cvtColor(imgthres, cv2.COLOR_GRAY2BGR)
 #cv2.drawContours(img, numero, -1, (0, 0,255), 1)
-cv2.drawContours(imgthres, contours, -1, (255, 0,0))
-imgH=cv2.resize(img, (300,300), interpolation=cv2.INTER_LINEAR)
-cv2.imshow("img", imgH)
-cv2.imshow("threshold", imgthres)
+#
+#imgH=cv2.resize(img, (300,300), interpolation=cv2.INTER_LINEAR)
+cv2.imshow("img", img)
+#cv2.imwrite("imagenes/imagenes_prueba1/sudoku_test6.jpg", img)
+#cv2.imshow("threshold", imgthres)
 cv2.waitKey(0)
